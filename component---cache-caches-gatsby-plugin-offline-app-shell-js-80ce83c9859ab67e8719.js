6d25005ec549f1579299a6483cf6d8345e50ad0b
ethani kitilah <ethani.kitilah@ega.go.tz>
2025-06-15 16:39:42 +0300

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-80ce83c9859ab67e8719.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkethan616=self.webpackChunkethan616||[]).push([[681],{8826:function(e,t,n){var u=n(4994);t.__esModule=!0,t.default=void 0;var a=u(n(6221)),r=u(n(6540)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,a.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),f=l;t.default=f}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-80ce83c9859ab67e8719.js.map
