f095da1ee2699dee56f4e290683b281b0a199b10
ethani kitilah <ethani.kitilah@ega.go.tz>
2025-06-16 08:57:08 +0300

    loader

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="/portfolio/ethan-caphace-cv.pdf" className="resume-link">
               Resume
             </a>
           </nav>
