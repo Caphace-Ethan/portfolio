f60b8f484b2b8171f9960a6d58fdf052ecdc49f8
ethani kitilah <ethani.kitilah@ega.go.tz>
2025-06-18 12:29:12 +0300

    isHome ??

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ const StyledContent = styled.div`
 `;
 
 const Layout = ({ children, location }) => {
-  const isHome = location.pathname === '/';
+  const isHome = location.pathname === '/portfolio/' || location.pathname === '/';
   const [isLoading, setIsLoading] = useState(isHome);
 
   // Sets target="_blank" rel="noopener noreferrer" on external links
